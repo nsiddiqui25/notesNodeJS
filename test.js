d1fe48ddd69a40e67da9a03180a721300b47cd0e
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2019-01-10 20:38:47 -0600

    messing with fs and https node-modules

--- test.js
+++ test.js
@@ -27,3 +27,24 @@ var myPhotoLocation = 'https://external-preview.redd.it/Ak4M0FYrZ3xSFVqVAjV45Gxa
 https.get(myPhotoLocation, function(response){
    response.pipe(fs.createWriteStream(__dirname + '/photo.jpg'));
 });
+
+//========================================//
+/*
+
+WHAT IS NPM? PACKAGE MANAGEMENT? WHY USE IT?
+npm - a centralized place where developers share their code with the world. A grocery store, of sorts, where we can find all our ingredients to build a web-app. 
+Package Managment - a package manager can automatically grab the most updated packages we'll need to use from a centralized location, instead of grabbing different tooks from different websites. we simply make a list, and our package manager grabs the most updated tools/techs we need
+
+WHAT TYPES OF FILES/PACKAGES CAN WE FIND ON NPM?
+2 Types of packages
+   1) node packages - packages of code that help us do things in node
+      -automation
+      -build tools
+      -server tasks
+   2) other things - some of these, below, are JavaScript and CSS packages that we need for our webapp to run in the browser
+      -jQuery
+      -Lodash
+      -Bootstrap
+      -normalize.css
+
+*/
